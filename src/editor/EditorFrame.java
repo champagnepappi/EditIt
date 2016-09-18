06087902128866c9f8fd88ba8479a2ce0b26aa97
kevin <kevinochieng548@gmail.com>
2016-09-18 21:19:53 +0300

    define actionPerformed for undo action

--- src/editor/EditorFrame.java
+++ src/editor/EditorFrame.java
@@ -752,7 +752,12 @@ return false;
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            //throw new UnsupportedOperationException("Not supported yet.");
+            if (this.isEnabled()) {
+                undoManager.undo();
+                undoAction.update();
+                redoAction.update();
+            }
         }
     }
 
